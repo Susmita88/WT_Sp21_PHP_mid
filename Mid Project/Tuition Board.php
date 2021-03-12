eb4825a223c13d27d3cb6c7848be306c4b4f44ae
Shushmita Chowdhury <anamica.chowdhury31@gmail.com>
2021-03-12 19:51:33 +0600

    Mid Project

---
+++ Mid Project/Tuition Board.php
@@ -0,0 +1,140 @@
+<html>
+ <head><title>Tuition Board</title>
+    <style>
+	    #header{
+			height:25px;
+			background-color:black;
+			position:fixed;
+			width:100%;
+			top:0;
+			left:0;
+			color:white
+		}
+		
+	    #p1div{
+			border:1px solid black;
+			margin:auto;
+			width:95%;
+			border-radius:10px;
+			padding-left:15px;
+			padding-right:15px;
+			padding-top:15px;
+			padding-bottom:15px;
+		}
+		
+		#p2div{
+			border:1px solid black;
+			margin:auto;
+			width:95%;
+			border-radius:10px;
+			padding-left:15px;
+			padding-right:15px;
+			padding-top:15px;
+			padding-bottom:15px;
+		}
+		
+		#p3div{
+			border:1px solid black;
+			margin:auto;
+			width:95%;
+			border-radius:10px;
+			padding-left:15px;
+			padding-right:15px;
+			padding-top:15px;
+			padding-bottom:15px;
+		}
+		
+		#p4div{
+			border:1px solid black;
+			margin:auto;
+			width:95%;
+			border-radius:10px;
+			padding-left:15px;
+			padding-right:15px;
+			padding-top:15px;
+			padding-bottom:15px;
+		}
+		
+		#p5div{
+			border:1px solid black;
+			margin:auto;
+			width:95%;
+			border-radius:10px;
+			padding-left:15px;
+			padding-right:15px;
+			padding-top:15px;
+			padding-bottom:15px;
+		}
+		
+		#footer{
+			height:15px;
+			background-color:black;
+			position:fixed;
+			width:100%;
+			bottom:0;
+			left:0;
+		}
+		
+	</style>
+ </head>
+ <body style="background-color:rgb(151, 117, 250);">
+    <div id="header"></div><br>
+    <H1 style="text-align:center;"> Tuition Board </H1>
+	<div id="p1div" style="background-color: white;">
+        <b>Need Bangla Medium Tutor for Class 2 Student - 5Days/Week</b><br>
+        <p>ID:1001   |   Mar 12,2021 </p>
+        <ul type="disc">
+            <li><b>Location:</b> Mirpur 13, Dhaka </li><br>                                
+	        <li><b>Subject:</b> All</li><br>
+	        <li><b>Salary:</b> 3000BDT<br>
+		     
+            <p style="text-align:right;"><a target="_blank" href="View Details 1.php">View Details</a></p>
+		
+	</div><br>
+	
+	<div id="p2div" style="background-color: white;">
+        <b>Need English Medium Tutor for Standard 5 Student - 4Days/Week</b><br>
+        <p>ID:1002  |   Feb 27,2021 </p>
+        <ul type="disc">
+            <li><b>Location:</b> Mohammadpur 15, Dhaka </li><br>                                
+	        <li><b>Subject:</b> All</li><br>
+	        <li><b>Salary:</b> 5000BDT<br>
+		
+		    <p style="text-align:right;"><a target="_blank" href="View Details 2.php">View Details</a></p>
+	</div><br>
+	
+	<div id="p3div" style="background-color: white;">
+        <b>Need Professional Skill Development Tutor for Computer Programming Student - 3Days/Week</b><br>
+        <p>ID:1003   |   Feb 15,2021 </p>
+        <ul type="disc">
+            <li><b>Location:</b> Khulshi 11, Chittagong </li><br>                                
+	        <li><b>Subject:</b> C, C++, Java</li><br>
+	        <li><b>Salary:</b> 7000BDT<br>
+			
+			 <p style="text-align:right;"><a target="_blank" href="View Details 3.php">View Details</a></p>
+	</div><br>
+	
+	<div id="p4div" style="background-color: white;">
+        <b>Need Religious Studies Tutor for Islamic Studies Student - 3Days/Week</b><br>
+        <p>ID:1004   |   Jan 12,2021 </p>
+        <ul type="disc">
+            <li><b>Location:</b> Shahjalal Upashahar G Block, Sylhet </li><br>                                
+	        <li><b>Subject:</b> Quran for Beginner</li><br>
+	        <li><b>Salary:</b> 3000BDT<br>
+			
+			<p style="text-align:right;"><a target="_blank" href="View Details 4.php">View Details</a></p>
+	</div><br>
+	
+	<div id="p5div" style="background-color: white;">
+        <b>Need Math Tutor for Class 9 Student - 3Days/Week</b><br>
+        <p>ID:1005   |   Jan 04,2021 </p>
+        <ul type="disc">
+            <li><b>Location:</b> Esa Khan Road, Narayanganj </li><br>                                
+	        <li><b>Subject:</b> Math</li><br>
+	        <li><b>Salary:</b> 4000BDT<br>
+			
+			<p style="text-align:right;"><a target="_blank" href="View Details 5.php">View Details</a></p>
+	</div><br>
+	<div id="footer"></div>
+ </body>
+</html>
---
+++ Mid Project/View Details 2.php
@@ -0,0 +1,49 @@
+<html>
+ <head><title>View Details</title>
+    <style>
+	    #header{
+			height:25px;
+			background-color:black;
+			position:fixed;
+			width:100%;
+			top:0;
+			left:0;
+			color:white
+		}
+		
+	    #p1div{
+			border:1px solid black;
+			margin:auto;
+			width:50%;
+			border-radius:10px;
+			padding-left:15px;
+			padding-top:15px;
+			padding-bottom:15px;
+		}
+		
+		
+	</style>
+ </head>
+ <body style="background-color:rgb(151, 117, 250);">
+    <div id="header"></div><br>
+    <H1 style="text-align:center;"> Need English Medium Tutor for Standard 5 Student - 4Days/Week </H1>
+	<p style="text-align:center;"><b>ID:1002</b></p><br>
+	<div id="p1div" style="background-color: white;">
+        <p>Posted Date: Feb 27,2021 </p>
+        <ul type="square">                               
+	        <li><b>Subjects:</b><br> All</li><br>
+			<li><b>No of Student: </b><br> 1</li><br>
+			<li><b>Student Gender: </b><br> Female</li><br>
+			<li><b>Preferred Tutor: </b><br> Female</li><br>
+			<li><b>Tutoring Days: </b><br> 4Days/Week</li><br>
+	        <li><b>Salary: </b><br> 5000BDT</li><br>
+			<li><b>Tutoring Time: </b><br> 5:00PM</li><br>
+			<li><b>Location: </b><br> Mohammadpur 15, Dhaka </li><br> 
+			<li><b>Other Requirements: </b><br> Interested tutors are requested to contact.</li><br>
+		<table><tr>
+		     <td colspan="2" align="right"><input type="submit" value="Apply" name="Apply"></td>
+		</tr></table>
+	</div><br>
+  
+ </body>
+</html>
